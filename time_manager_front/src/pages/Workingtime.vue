5bbed2b130a8e0385e3a36cf218c938a44ee6dd6
Fahad <fahad@MacBook-Air-de-Fahad.local>
2024-10-11 13:36:07 +0200

    quick change display workingtime

--- time_manager_front/src/pages/Workingtime.vue
+++ time_manager_front/src/pages/Workingtime.vue
@@ -4,14 +4,6 @@
       <button class="btn btn-light" @click="goToHome">
         <i class="fas fa-home"></i> Home
       </button>
-
-      <button
-        class="btn btn-success"
-        data-bs-toggle="modal"
-        data-bs-target="#addWorkingTimeModal"
-      >
-        <i class="fas fa-plus"></i> Ajouter un Working Time
-      </button>
     </div>
 
     <h1 class="text-center mb-5">Gestion du temps de travail</h1>
@@ -38,85 +30,22 @@
 
     <div v-if="workingTimes.length > 0" class="mt-5">
       <h3 class="text-center">Working Times</h3>
-      <div class="row">
-        <div
-          v-for="workingTime in workingTimes"
-          :key="workingTime.id"
-          class="col-md-4 mb-3"
-        >
-          <div class="card">
-            <div class="card-body">
-              <p class="card-text">Début : {{ workingTime.start }}</p>
-              <p class="card-text">Fin : {{ workingTime.end_w }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div
-      class="modal fade"
-      id="addWorkingTimeModal"
-      tabindex="-1"
-      aria-labelledby="addWorkingTimeModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="addWorkingTimeModalLabel">
-              Ajouter un Working Time
-            </h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <form @submit.prevent="addWorkingTime">
-              <div class="mb-3">
-                <label for="selectUser" class="form-label"
-                  >Sélectionner un utilisateur</label
-                >
-                <select
-                  id="selectUser"
-                  class="form-control"
-                  v-model="newWorkingTime.user_id"
-                  required
-                >
-                  <option v-for="user in users" :key="user.id" :value="user.id">
-                    {{ user.username }}
-                  </option>
-                </select>
-              </div>
-
-              <div class="mb-3">
-                <label for="startTime" class="form-label">Début</label>
-                <input
-                  type="datetime-local"
-                  class="form-control"
-                  id="startTime"
-                  v-model="newWorkingTime.start"
-                  required
-                />
-              </div>
-              <div class="mb-3">
-                <label for="endTime" class="form-label">Fin</label>
-                <input
-                  type="datetime-local"
-                  class="form-control"
-                  id="endTime"
-                  v-model="newWorkingTime.end"
-                  required
-                />
-              </div>
-              <button type="submit" class="btn btn-primary">Ajouter</button>
-            </form>
-          </div>
-        </div>
-      </div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Heure de départ</th>
+            <th>Heure d'arrivée</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="workingTime in workingTimes" :key="workingTime.id">
+            <td>{{ new Date(workingTime.start).toLocaleDateString() }}</td>
+            <td>{{ new Date(workingTime.start).toLocaleTimeString() }}</td>
+            <td>{{ new Date(workingTime.end_w).toLocaleTimeString() }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -125,7 +54,6 @@
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import axios from "axios";
-import * as bootstrap from "bootstrap";
 
 export default {
   name: "WorkingTimePage",
@@ -134,11 +62,6 @@ export default {
     const searchQuery = ref("");
     const users = ref([]);
     const workingTimes = ref([]);
-    const newWorkingTime = ref({
-      start: "",
-      end: "",
-      user_id: null,
-    });
     const alertMessage = ref("");
 
     const goToHome = () => {
@@ -194,59 +117,15 @@ export default {
       }, 3000);
     };
 
-    const addWorkingTime = async () => {
-      if (!newWorkingTime.value.user_id) {
-        return;
-      }
-
-      try {
-        await axios.post(
-          `http://localhost:4000/api/workingtime/${newWorkingTime.value.user_id}`,
-          {
-            workingtime: {
-              start: newWorkingTime.value.start,
-              end_w: newWorkingTime.value.end,
-            },
-            userId: newWorkingTime.value.user_id,
-          }
-        );
-
-        const modalElement = document.getElementById("addWorkingTimeModal");
-        const modalInstance = bootstrap.Modal.getInstance(modalElement);
-        if (modalInstance) {
-          modalInstance.hide();
-        }
-
-        newWorkingTime.value = {
-          start: "",
-          end: "",
-          user_id: null,
-        };
-      } catch (error) {
-        // Intentionally left empty
-      }
-    };
-
-    const fetchAllUsers = async () => {
-      try {
-        const response = await axios.get("http://localhost:4000/api/users");
-        users.value = response.data.data || [];
-      } catch (error) {
-        // Intentionally left empty
-      }
-    };
-
-    onMounted(fetchAllUsers);
+    onMounted(() => {});
 
     return {
       goToHome,
       searchQuery,
       users,
       workingTimes,
-      newWorkingTime,
       searchUsers,
       getWorkingTimes,
-      addWorkingTime,
       alertMessage,
     };
   },
